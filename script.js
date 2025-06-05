277ff88a94e315cc4bf2de680ea7fbf2bbb56703
igs111 <iqciuplay@gmail.com>
2025-06-05 18:58:58 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// script.js
-function animateBox() {
-  const box = document.getElementById('box');
-  box.style.transform = `translateY(${Math.random() * 300}px) rotate(${Math.random() * 360}deg)`;
-}
